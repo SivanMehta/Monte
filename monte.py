6540cc85ee44183f385ae3c94cb6929e6e610622
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-08-11 16:43:44 -0700

    added thread generation on object creation, rather than test start

--- monte.py
+++ monte.py
@@ -1,8 +1,8 @@
-import webbrowser
-
-from matplotlib import pyplot as plt
 import random
 import psutil
+import sys
+import webbrowser
+from matplotlib import pyplot as plt
 from Queue import Queue
 from threading import Thread
 
@@ -18,10 +18,40 @@ class Monte():
         self.domain = domain
         self.trials = trials
 
-        self.data = None
+        self.data = []
 
         if type(self.domain) != list or type(self.trials) != int: 
             raise TypeError
+
+        self.q = Queue(maxsize=0)
+        # generally recommended to have a max of 4 threads per core (according to by boss)
+        num_threads = psutil.cpu_count() * 4
+
+        for i in xrange(num_threads):
+            worker = Thread(target = self.do_trial)
+            worker.setDaemon(True)
+            worker.start()
+            sys.stdout.flush()
+            sys.stdout.write("\rStarted %d threads... " % (i + 1))
+        print "all threads started!"
+
+    def do_trial(self):
+        while True:
+            x = self.q.get()
+
+            outcome = self.func(x)
+            self.data.append(outcome)
+
+            self.q.task_done()
+
+    def test_multi(self):
+        for i in xrange(self.trials):
+            choice = random.choice(self.domain)
+            self.q.put(choice)
+
+        self.q.join()
+
+        return self.data
 
     def test(self):
         outcomes = []
@@ -34,14 +64,14 @@ class Monte():
         return outcomes
 
     def plot(self):
-        if self.data == None:
+        if len(self.data) == 0:
             raise RuntimeError("Test has not been run yet")
 
         plt.hist(self.data, bins = 20)
         plt.show()
 
     def __repr__(self):
-        return "self.func: %s\nself.domain: %s\nself.trails: %d" \
+        return "self.func: %s\nself.domain: %s\nself.trials: %d" \
         % (self.func.__name__, str(self.domain), self.trials)
 
 
