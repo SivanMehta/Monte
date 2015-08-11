28fc8d0c614511e18678ae26642d8d279433d410
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-08-11 16:44:07 -0700

    added timing example

--- example.py
+++ example.py
@@ -1,12 +1,25 @@
-import math
+import math, time
 from monte import Monte
 
-a = range(-1000, 1000)
-a = map(lambda x: x/1000.0, a)
+def f(x):
+    time.sleep(.01)
+    return math.sin(x)
+
+domain = range(-1000, 1000)
+domain = map(lambda x: x/1000.0, a)
 # [-1.0, -0.999, -0.998, -0.997, -0.996 ...
 # 0.995, 0.996, 0.997, 0.998, 0.999]
-trials = 1000
+trials = 10000
 
-a = Monte(math.sin, a, trials)
-results = a.test()
-print(sum(results)/trials)
+tester = Monte(f, a, trials)
+
+print "Multithreaded:"
+start = time.time()
+results = tester.test_multi()
+print "\t", time.time() - start
+
+
+print "Singlethreaded:"
+start = time.time()
+results = tester.test()
+print "\t", time.time() - start
